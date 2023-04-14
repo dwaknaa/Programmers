95504f6c8f9be8b05b7fee6a4649d2a0bf13050a
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2023-04-14 17:12:56 +0900

    더 맵게 수정(정확성 100 효율성 0)

--- Programmers/더맵게.cpp
+++ Programmers/더맵게.cpp
@@ -5,29 +5,28 @@ using namespace std;
 
 int solution(vector<int> scoville, int K) {
     int answer = 0;
-    auto isUnder = [&K](int i) { return K >= i; };
+    auto isUnder = [&K](int i) { return K > i; };
 
     while(1)
     {
+        sort(scoville.begin(), scoville.end());
+
+        if(scoville.size() == 1 && scoville.front() < K) return -1;
         int under = count_if(scoville.begin(), scoville.end(), isUnder);
 
-        if(under > 0)
+        if(under == 0)
         {
-            answer++;
-        }
-        else
-        {
-            break;
+            return answer;
         }
         
-        sort(scoville.begin(), scoville.end());
-    
         int first = scoville.front();
         scoville.erase(scoville.begin());
         int second = scoville.front();
         scoville.erase(scoville.begin());
 
         scoville.push_back(first + (second*2));
+        
+        answer++;
     }
     
     return answer;
