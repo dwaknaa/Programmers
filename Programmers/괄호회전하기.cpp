580a7ec6dcc782467186416c10afb2a27a3c0121
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2023-06-01 20:02:03 +0900

    괄호회전하기 정답

--- Programmers/괄호회전하기.cpp
+++ Programmers/괄호회전하기.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
+#include <stack>
 using namespace std;
 
 int solution(string s) {
@@ -9,33 +9,57 @@ int solution(string s) {
     
     for(int i=0; i<s.length()-1; i++)
     {
-        string temp = s;
-        while(1)
+        stack<char> st;
+        bool right = true;
+        for(int j=0; j<s.length(); j++)
         {
-            bool check = false;
-            if(temp.find("{}") != string::npos)
+            if(s[j] == '[' || s[j] == '{' || s[j] == '(')
             {
-                temp.erase(temp.find("{}"), 2);
-                check = true;
+                st.push(s[j]);
             }
-            if(temp.find("[]") != string::npos)
+            else if(s[j] == ']')
             {
-                temp.erase(temp.find("[]"), 2);
-                check = true;
+                if(st.top() == '[')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    right = false;
+                    break;
+                }
             }
-            if(temp.find("()") != string::npos)
+            else if(s[j] == '}')
             {
-                temp.erase(temp.find("()"), 2);
-                check = true;
+                if(st.top() == '{')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    right = false;
+                    break;
+                }
             }
-            
-            if(!check || temp.length() == 0) break;
+            else if(s[j] == ')')
+            {
+                if(st.top() == '(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    right = false;
+                    break;
+                }
+            }
         }
         
-        if(temp.length() == 0) answer++;
+        if(right && st.empty()) answer++;
      
-        string last = s.substr(s.length()-1, 1);
-        s = last + s.substr(0, s.length()-1);
+        string first = s.substr(0, 1);
+        s.erase(s.begin());
+        s += first;
     }
     
     return answer;
