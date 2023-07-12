c4ad84757893bc57a057cc90a3e98eea253722b2
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2023-07-12 21:14:24 +0900

    프로그래머스 주차 요금 계산 첫번째 풀이

---
+++ Programmers/주차요금계산.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+using namespace std;
+
+int getParkingTime(string startTime, string endTime)
+{
+    int sum = 0;
+    auto index = startTime.find(":");
+    int startHour = stoi(startTime.substr(0, index));
+    int startMin = stoi(startTime.substr(index+1));
+    
+    index = endTime.find(":");
+    int endHour = stoi(endTime.substr(0, index));
+    int endMin = stoi(endTime.substr(index+1));
+    
+    if(endMin >= startMin)
+    {
+        sum += (endHour-startHour)*60;
+        sum += endMin-startMin;
+    }
+    else
+    {
+        sum += (endHour-1-startHour)*60;
+        sum += (60-startMin)+endMin;
+    }
+    return sum;
+}
+
+vector<int> solution(vector<int> fees, vector<string> records) {
+    vector<int> answer;
+    map<string, string> recordMap;
+    map<string, int> tempMap;
+    
+    for(int i=0; i<records.size(); i++)
+    {
+        string record = records[i];
+        auto index = record.find(" ");
+        string time = record.substr(0, index);
+        record = record.substr(index+1);
+        index = record.find(" ");
+        string carNum = record.substr(0, index);
+        
+        auto ret = recordMap.insert({carNum, time});
+        if(!ret.second)
+        {
+            int parkingTime = getParkingTime(recordMap[carNum], time);
+            auto ret2 = tempMap.insert({carNum, parkingTime});
+            if(!ret2.second) tempMap[carNum] += parkingTime;
+            recordMap.erase(carNum);
+        }
+    }
+    
+    for(auto iter=recordMap.begin(); iter!= recordMap.end(); iter++)
+    {
+        int parkingTime = getParkingTime(iter->second, "23:59");
+        auto ret2 = tempMap.insert({iter->first, parkingTime});
+        if(!ret2.second) tempMap[iter->first] += parkingTime;
+    }
+    
+    for(auto iter=tempMap.begin(); iter!=tempMap.end(); iter++)
+    {
+        if(iter->second > fees[0])
+        {
+            if(iter->second-fees[0] > fees[2])
+            {
+                if((iter->second-fees[0])%fees[2]==0)
+                {
+                   answer.push_back(fees[1]+((iter->second-fees[0])/fees[2])*fees[3]);
+                }
+                else
+                {
+                   answer.push_back(fees[1]+(((iter->second-fees[0])/fees[2])+1)*fees[3]);
+                }
+            }
+            else
+            {
+                answer.push_back(fees[1]+fees[3]);
+            }
+        }
+        else
+        {
+            answer.push_back(fees[1]);
+        }
+    }
+    
+    return answer;
+}
