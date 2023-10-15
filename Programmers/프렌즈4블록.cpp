d7427cc86ce7c8fa28f0b021bfd14f3f082cc93d
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2023-10-15 13:30:18 +0900

    1015

---
+++ Programmers/프렌즈4블록.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int solution(int m, int n, vector<string> board) {
+    int answer = 0;
+    
+    while(1)
+    {
+        set<pair<int, int>> s;
+
+        for(int i=0; i<m-1; i++)
+        {
+            string xStr = board[i];
+
+            for(int j=0; j<n-1; j++)
+            {
+                if(xStr[j]=='@') continue;
+                
+                if(xStr[j]!=xStr[j+1]) continue;
+                if(xStr[j]!=board[i+1][j]) continue;
+                if(xStr[j]!=board[i+1][j+1]) continue;
+
+                s.insert(make_pair(i, j));
+                s.insert(make_pair(i, j+1));
+                s.insert(make_pair(i+1, j));
+                s.insert(make_pair(i+1, j+1));
+            }
+        }
+
+        if(s.size()==0) break;
+        
+        for(auto iter=s.begin(); iter!=s.end(); iter++)
+        {
+            auto p = *iter;
+            board[p.first][p.second] = '@';
+        }
+
+        answer += s.size();
+        
+        for(auto iter=board.begin(); iter!=board.end(); iter++)
+        {
+            string str = *iter;
+            cout << str << endl;
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(board[j][i]=='@')
+                {
+                    for(int k=j; k>0; k--)
+                    {
+                        board[k][i] = board[k-1][i];
+                    }
+                    
+                    board[0][i] = '@';
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
