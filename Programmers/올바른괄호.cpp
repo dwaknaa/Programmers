e69f207dfda9b07828b3d410e20b3315e501b16a
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2023-11-29 23:37:53 +0900

    올바른괄호 재풀이 - 코드라인 간소화

--- Programmers/올바른괄호.cpp
+++ Programmers/올바른괄호.cpp
@@ -1,42 +1,21 @@
-#include <string>
+#include<string>
 #include <iostream>
 #include <stack>
-#include <algorithm>
 using namespace std;
 
 bool solution(string s)
 {
     bool answer = true;
-    stack<string> strStack;
+    int endCnt = 0;
     
-    int startCnt = count(s.begin(), s.end(), '(');
-    int endCnt = count(s.begin(), s.end(), ')');
-
-    if(startCnt != endCnt) return false;
-    
-    int startParen = 0;
-    int endParen = 0;
-    
-    for(int i=s.length()-1; i>=0; i--)
+    for(int i=s.size()-1; i>=0; i--)
     {
-        if(s[i] == ')')
-        {
-            endParen++;
-        }
-        else
-        {
-            if(endParen == 0) return false;
-            startParen++;
-
-            if(startParen > endParen) return false;
-        }
+        if(s[i] == ')') endCnt++;
+        else endCnt--;
         
-        if(startParen == endParen)
-        {
-            startParen = 0;
-            endParen = 0;
-        }
+        if(endCnt < 0) return false;
     }
-    
+
+    if(endCnt > 0) answer = false;
     return answer;
 }
