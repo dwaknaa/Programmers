1fc5ed03e51a6938be8dc8af6a9b3271aca8d31e
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2024-04-25 21:32:42 +0900

    0425

--- Programmers/디펜스게임.cpp
+++ Programmers/디펜스게임.cpp
@@ -1,47 +1,38 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
-
-vector<int> resultVec;
-
-void func(int n, int k, vector<int> vec, int index)
-{
-    if(index < vec.size())
-    {
-        if(k > 0)
-        {
-            func(n, k-1, vec, index+1);
-        }
-        
-        if(vec[index] <= n)
-        {
-            func(n-vec[index], k, vec, index+1);
-        }
-        else
-        {
-            if(k==0)
-            {
-                resultVec.push_back(index);
-                return;
-            }
-        }
-    }
-    else
-    {
-        resultVec.push_back(index);
-    }
-}
 
 int solution(int n, int k, vector<int> enemy) {
     int answer = 0;
+    multiset<int, greater<int>> s;
     
-    func(n, k, enemy, 0);
-    
-    if(resultVec.size() > 0)
+    while(1)
     {
-        sort(resultVec.begin(), resultVec.end());
-        answer = resultVec.back();
+        if(answer >= enemy.size()) break;
+
+        if(enemy[answer] <= n)
+        {
+            n -= enemy[answer];
+            s.insert(enemy[answer]);
+            answer++;
+        }
+        else
+        {
+            if(k==0) break;
+            if(s.empty() || *s.begin() < enemy[answer])
+            {
+                k-=1;
+                answer++;
+            }
+            else
+            {
+                n += *s.begin();
+                s.erase(s.begin());
+                k-=1;
+            }
+        }
     }
     
     return answer;
