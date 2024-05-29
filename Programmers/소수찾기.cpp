1113e5b788178fc3d0c0653b936f3f09beea58ab
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2024-05-29 23:17:27 +0900

    0529

--- Programmers/소수찾기.cpp
+++ Programmers/소수찾기.cpp
@@ -1,48 +1,28 @@
 #include <string>
-#include <algorithm>
 #include <vector>
-#include <set>
 #include <cmath>
 using namespace std;
 
-bool isPrime(int n)
+bool isPrime(int number)
 {
-    if(n<=1) return false;
+    if(number==2) return true;
+    if(number==1 | number%2==0) return false;
     
-    for (int i = 2; i <= sqrt(n); i++)
+    for(int i=3; i<=sqrt(number); i+=2)
     {
-        if (n%i == 0)
-        {
-            return false;
-        }
+        if(number%i==0) return false;
     }
     
     return true;
 }
 
-int solution(string numbers) {
+int solution(int n) {
     int answer = 0;
     
-    sort(numbers.begin(), numbers.end());
-    set<string> nums;
-    set<int> answers;
-    int n = numbers.size();
-    string sub;
-    do {
-        for (int i = 1; i <= n; i++)
-        {
-            sub = numbers.substr(0, i);
-            nums.insert(sub);
-            
-            int num = stoi(sub);
-            if(answers.find(num)==answers.end() && isPrime(num))
-            {
-                answers.insert(num);
-            }
-        }
-    } while (next_permutation(numbers.begin(), numbers.end()));
-    
-    answer = answers.size();
+    for(int i=2; i<=n; i++)
+    {
+        if(isPrime(i)) answer++;
+    }
     
     return answer;
 }
