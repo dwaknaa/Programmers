501b33bdf85df9a3063e226e7c3ed41af5055fd1
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2024-09-19 20:28:19 +0900

    240919 올바른 괄호

--- Programmers/올바른괄호.cpp
+++ Programmers/올바른괄호.cpp
@@ -1,21 +1,30 @@
 #include<string>
-#include <iostream>
-#include <stack>
+
 using namespace std;
 
 bool solution(string s)
 {
     bool answer = true;
-    int endCnt = 0;
-    
-    for(int i=s.size()-1; i>=0; i--)
-    {
-        if(s[i] == ')') endCnt++;
-        else endCnt--;
-        
-        if(endCnt < 0) return false;
-    }
 
-    if(endCnt > 0) answer = false;
+    // 여는 괄호 개수 확인을 위한 변수
+    int openParenCnt = 0;
+    
+    // 문자열 s를 맨 앞글자부터 검토
+    for(int i=0; i<s.length(); i++)
+    {
+        // 여는 괄호보다 닫는 괄호가 많이 나온 부분 문자열일 경우
+        if(openParenCnt < 0)
+        {
+            answer = false;
+            break;
+        }
+        
+        if(s.substr(i, 1)=="(") openParenCnt++;
+        else openParenCnt--;
+    }
+    
+    // 괄호들이 바르게 짝지어져 있지 않은 경우
+    if(openParenCnt != 0) answer = false;
+    
     return answer;
 }
