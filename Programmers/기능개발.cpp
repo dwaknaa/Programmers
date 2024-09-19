2f61138dc067397a8d821e61c34f3b20f060372c
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2024-09-19 20:14:06 +0900

    240919 기능개발

--- Programmers/기능개발.cpp
+++ Programmers/기능개발.cpp
@@ -1,54 +1,40 @@
 #include <string>
 #include <vector>
-
 using namespace std;
 
-vector<int> solution(vector<int> progresses, vector<int> speeds)
+int getNeedDays(int progresses, int speeds)
 {
-    vector<int> answer;
-    vector<int> temp;
-    
-    // 기능별로 speed에 따른 완료일 계산
-    for(int i = 0; i < static_cast<int>(progresses.size()); i++)
-    {
-        int days;
-        if((100-progresses[i]) > speeds[i])
-        {
-            if(((100-progresses[i]) % speeds[i]) == 0)
-            {
-                days = (100-progresses[i]) / speeds[i];
-            }
-            else
-            {
-                // 남은 진도가 speed로 딱 맞게 나누어지지 않으면 며칠 하고도 몇시간이 더 소요 되는데
-                // 배포는 하루의 끝에 이루어지므로 완료일까지 하루가 더 필요함
-                days = ((100-progresses[i]) / speeds[i])+1;
-            }
-        }
-        else // 기능별 남은 진도가 speed보다 작거나 같을 때는 최대 하루면 완료
-        {
-            days = 1;
-        }
+    int days = (100-progresses) / speeds;
+    int hours = (100-progresses) % speeds;
         
-        temp.push_back(days);
-    }
+    if(hours > 0) days+=1;
     
+    return days;
+}
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    
+    int beforeDays = getNeedDays(progresses[0], speeds[0]);
     int count = 1;
-    int distributeDay = temp[0]; // 앞의 배포일과 비교해야 하므로 메모리에 저장해둬야 함
-    for(int j = 1; j < static_cast<int>(temp.size()); j++)
+    for(int i=1; i<progresses.size(); i++)
     {
-        if(temp[j] <= distributeDay)
+        int days = getNeedDays(progresses[i], speeds[i]);
+        
+        if(beforeDays < days) // 이전 기능의 작업 종료일보다 이후에 끝날 때
+        {
+            // 이전 작업들 먼저 배포 진행
+            answer.push_back(count);
+            
+            // 새로운 배포날이 정해졌으므로 각 값 초기화
+            count = 1;
+            beforeDays = days;
+        }
+        else // 이전 기능의 작업 종료일보다 이전에 끝날 때
         {
             count++;
         }
-        else
-        {
-            answer.push_back(count);
-            count = 1;
-            distributeDay = temp[j];
-        }
     }
-    
     answer.push_back(count);
     
     return answer;
