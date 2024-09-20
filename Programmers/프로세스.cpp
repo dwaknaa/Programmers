9aba30e5d28f6fc4bc3f29b15fe8890a0723eb7a
dwaknaa <128336371+dwaknaa@users.noreply.github.com>
2024-09-21 01:53:43 +0900

    프로세스 240920

--- Programmers/프로세스.cpp
+++ Programmers/프로세스.cpp
@@ -1,44 +1,50 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <algorithm>
-#include <iostream>
+#include <set>
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    queue<int> que;
+    
     queue<int> indexQue;
+    multiset<int, greater<int>> sortedSet;
     for(int i=0; i<priorities.size(); i++)
     {
-        que.push(priorities[i]);
         indexQue.push(i);
+        sortedSet.insert(priorities[i]);
     }
     
-    sort(priorities.begin(), priorities.end());
-
     while(1)
     {
-        if(que.front() == priorities.back())
+        int num = priorities.front();
+        priorities.erase(priorities.begin());
+        
+        int index = indexQue.front();
+        indexQue.pop();
+
+        int maxNum = *(sortedSet.begin());
+        if (num != maxNum)
         {
-            que.pop();
-            answer++;
-            if(indexQue.front() == location)
-            {
-                break;
-            }
-            indexQue.pop();
-            priorities.pop_back();
-        }
-        else
-        {
-            int num = que.front();
-            que.pop();
-            que.push(num);
-            int index = indexQue.front();
-            indexQue.pop();
+            // 이번에 꺼낸 프로세스의 우선순위가
+            // 현재 기준 가장 높은 우선순위가 아닌 경우 실행하지 않고 다시 넣음
+            priorities.push_back(num);
             indexQue.push(index);
+            
+            continue;
         }
+        
+        // 실행 횟수 +1
+        answer++;
+
+        // 목표 프로세스가 실행된 경우 반복문 종료
+        if(index == location)
+        {
+            break;
+        }
+        
+        // 실행한 프로세스의 우선순위 정보 삭제
+        sortedSet.erase(sortedSet.begin());
     }
     
     return answer;
